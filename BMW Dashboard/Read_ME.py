7ed6dc6665a0f732bfc8d80d740c2ab71777ce0e
Jiajie Lin <159084538+Jiajie-Lin@users.noreply.github.com>
2025-12-10 12:33:18 -0500

    Implement read_me function in Read_ME.py
    Add read_me function to provide project overview

---
+++ BMW Dashboard/Read_ME.py
@@ -0,0 +1,3 @@
+def read_me():
+    text = (" The Box and Whisker whisker are all different and takes in the data of Engine Size, Mileage, Price and Sales Volume but the data is almost the exact the same for each year. The Sankey Diagram is based off the of the class template and is adjusted by Search.")
+    return text
